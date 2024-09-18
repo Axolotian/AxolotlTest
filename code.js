598b0330db4cbf877f19e5abc51bdf8698b9f503
Axolotian <shum_eu_xuan_ethan@students.edu.sg>
2024-09-18 12:51:34 +0800

    Update code.js

--- code.js
+++ code.js
@@ -2,6 +2,7 @@ class HyperTest {
   constructor() {
     this.pyodideReady = false; // Flag to check if Pyodide is initialized
     this.pyodideLoading = false;
+    this.pyodideLoadingStatus = "0/5"
     this.pythonCode = `
 import random
 test = random.randint(1, 10)
@@ -34,6 +35,11 @@ test
           opcode:  'checkPythonLoading',
           blockType: Scratch.BlockType.BOOLEAN,
           text: 'Python Loading?'
+        },
+        {
+          opcode: 'pythonLoadingStep',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: 'Python Loading Status'
         }
       ],
     };
@@ -42,17 +48,23 @@ test
   async preloadPyodide() {
     if (typeof pyodide === 'undefined') {
       this.pyodideLoading = true;
+      this.pyodideLoadingStatus = "0/5";
       const pyodideScript = document.createElement('script');
+      this.pyodideLoadingStatus = "1/5";
       pyodideScript.src = 'https://cdn.jsdelivr.net/pyodide/v0.18.1/full/pyodide.js';
+      this.pyodideLoadingStatus = "2/5";
       document.head.appendChild(pyodideScript);
+      this.pyodideLoadingStatus = "3/5";
 
       await new Promise((resolve, reject) => {
         pyodideScript.onload = resolve;
         pyodideScript.onerror = reject;
       });
+      this.pyodideLoadingStatus = "4/5";
 
       // Wait for Pyodide to fully load
       await loadPyodide();
+      this.pyodideLoadingStatus = "5/5";
       this.pyodideLoading = false;
       this.pyodideReady = true; // Mark Pyodide as loaded
     }
@@ -84,6 +96,9 @@ test
   }
   checkPythonLoading() {
     return this.pyodideLoading;
+  }
+  pythonLoadingStep() {
+    return this.pyodideLoadingStatus
   }
 }
 
